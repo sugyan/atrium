91babb8f45c45c51176756a07b6f0010d84a1aa0
sugyan <sugi1982@gmail.com>
2023-04-18 17:09:16 +0900

    Remove unused codes

--- atprs-xrpc/src/lib.rs
+++ atprs-xrpc/src/lib.rs
@@ -1,18 +1,3 @@
 mod client;
 
 pub use client::*;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
