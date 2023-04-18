69bafa8c761a7c443e350edfa010bc055ee0113c
sugyan <sugi1982@gmail.com>
2023-04-18 22:59:03 +0900

    Update xrpc client

--- atprs-cli/src/main.rs
+++ atprs-cli/src/main.rs
@@ -1,4 +1,4 @@
-use atprs_xrpc::{Client, CreateRecordInput, CreateSessionInput, CreateSessionOutput, Record};
+use atprs_xrpc::{Client, CreateRecordInput, CreateSessionInput, Record};
 use clap::{Parser, Subcommand};
 use std::fs;
 use std::path::PathBuf;
@@ -6,6 +6,8 @@ use toml::{Table, Value};
 
 #[derive(Parser, Debug)]
 struct Args {
+    #[arg(short, long, default_value = "https://bsky.social")]
+    pds_host: String,
     #[arg(short, long, default_value = "config.toml")]
     config: PathBuf,
     #[command(subcommand)]
@@ -26,7 +28,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let (Some(Value::String(identifier)), Some(Value::String(password))) =
         (value.get("identifier"), value.get("password"))
     {
-        run(identifier, password, args.command).await?;
+        run(args.pds_host, identifier, password, args.command).await?;
     } else {
         panic!("invalid config");
     }
@@ -34,43 +36,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn run(
+    host: String,
     identifier: &str,
     password: &str,
     command: Command,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client {};
+    let mut client = Client::new(host);
     let session = client
         .create_session(CreateSessionInput {
             identifier: identifier.to_string(),
             password: password.to_string(),
         })
-        .await?
-        .error_for_status()?
-        .json::<CreateSessionOutput>()
         .await?;
-    let result = match command {
+    client.set_auth(session.clone());
+    match command {
         Command::GetProfile => {
-            client
-                .get_profile(session.handle, session.access_jwt)
-                .await?
-                .json::<serde_json::Value>()
-                .await?
+            println!("{:?}", client.get_profile(session.handle).await?);
         }
         Command::CreateRecord { text } => {
-            client
-                .create_record(
-                    CreateRecordInput {
+            println!(
+                "{:?}",
+                client
+                    .create_record(CreateRecordInput {
                         repo: session.did,
                         collection: String::from("app.bsky.feed.post"),
                         record: Record::FeedPost(text),
-                    },
-                    session.access_jwt,
-                )
-                .await?
-                .json::<serde_json::Value>()
-                .await?
+                    })
+                    .await?
+            )
         }
     };
-    println!("{result:?}");
+
     Ok(())
 }
--- atprs-xrpc/src/client.rs
+++ atprs-xrpc/src/client.rs
@@ -1,12 +1,52 @@
 use chrono::Utc;
-use reqwest::{Error, Response};
+use reqwest::Error;
 use serde::{Deserialize, Serialize};
-use std::{fmt::Debug, future::Future};
+use std::fmt::Debug;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AuthInfo {
+    #[serde(rename = "accessJwt")]
+    pub access_jwt: String,
+    #[serde(rename = "refreshJwt")]
+    pub refresh_jwt: String,
+    pub handle: String,
+    pub did: String,
+}
 
 #[derive(Debug, Serialize)]
 pub struct CreateSessionInput {
     pub identifier: String,
     pub password: String,
+}
+
+pub type CreateSessionOutput = AuthInfo;
+
+#[derive(Debug, Deserialize)]
+pub struct GetProfileOutput {
+    pub did: String,
+    pub handle: String,
+    #[serde(rename = "displayName")]
+    pub display_name: Option<String>,
+    pub description: Option<String>,
+    pub avatar: Option<String>,
+    pub banner: Option<String>,
+    #[serde(rename = "followersCount")]
+    pub followers_count: Option<u32>,
+    #[serde(rename = "followsCount")]
+    pub follows_count: Option<u32>,
+    #[serde(rename = "postsCount")]
+    pub posts_count: Option<u32>,
+    #[serde(rename = "indexedAt")]
+    pub indexed_at: Option<String>,
+    pub viewer: Option<ViewerState>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ViewerState {
+    pub muted: Option<bool>,
+    pub following: Option<String>,
+    #[serde(rename = "followedBy")]
+    pub followed_by: Option<String>,
 }
 
 #[derive(Debug, Serialize)]
@@ -48,51 +88,72 @@ pub struct CreateRecordInput {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct CreateSessionOutput {
-    #[serde(rename = "accessJwt")]
-    pub access_jwt: String,
-    #[serde(rename = "refreshJwt")]
-    pub refresh_jwt: String,
-    pub handle: String,
-    pub did: String,
+pub struct CreateRecordOutput {
+    pub uri: String,
+    pub cid: String,
 }
 
-#[derive(Debug)]
-pub struct Client {}
+#[derive(Debug, Default)]
+pub struct Client {
+    host: String,
+    auth: Option<AuthInfo>,
+}
 
 impl Client {
-    pub fn create_session(
+    pub fn new(host: String) -> Self {
+        Self {
+            host,
+            ..Default::default()
+        }
+    }
+    pub fn set_auth(&mut self, auth: AuthInfo) {
+        self.auth = Some(auth);
+    }
+    pub async fn create_session(
         &self,
         input: CreateSessionInput,
-    ) -> impl Future<Output = Result<Response, Error>> {
+    ) -> Result<CreateSessionOutput, Error> {
         reqwest::Client::new()
-            .post(
-                "https://bsky.social/xrpc/com.atproto.server.createSession
-            ",
-            )
+            .post(format!(
+                "{}/xrpc/com.atproto.server.createSession",
+                self.host
+            ))
             .json(&input)
             .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await
     }
-    pub fn get_profile(
-        &self,
-        actor: String,
-        token: String,
-    ) -> impl Future<Output = Result<Response, Error>> {
-        reqwest::Client::new()
-            .get("https://bsky.social/xrpc/app.bsky.actor.getProfile")
-            .bearer_auth(token)
+    pub async fn get_profile(&self, actor: String) -> Result<GetProfileOutput, Error> {
+        let mut builder =
+            reqwest::Client::new().get(format!("{}/xrpc/app.bsky.actor.getProfile", self.host));
+        if let Some(auth_info) = &self.auth {
+            builder = builder.bearer_auth(&auth_info.access_jwt);
+        }
+        builder
             .query(&[("actor", actor)])
             .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await
     }
-    pub fn create_record(
+    pub async fn create_record(
         &self,
         input: CreateRecordInput,
-        token: String,
-    ) -> impl Future<Output = Result<Response, Error>> {
-        reqwest::Client::new()
-            .post("https://bsky.social/xrpc/com.atproto.repo.createRecord")
-            .bearer_auth(token)
+    ) -> Result<CreateRecordOutput, Error> {
+        let mut builder = reqwest::Client::new()
+            .post(format!("{}/xrpc/com.atproto.repo.createRecord", self.host));
+        if let Some(auth_info) = &self.auth {
+            builder = builder.bearer_auth(&auth_info.access_jwt)
+        }
+        builder
             .json(&input)
             .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await
     }
 }
