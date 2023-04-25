11d88f0ac5ec5c06e77ab8f790f11422a74076ac
sugyan <sugi1982@gmail.com>
2023-04-25 23:38:33 +0900

    Add `lexgen`

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # atprs
+
+[AT Protocol](https://atproto.com/) Rust implementation
+
+WIP
+
+```sh
+cargo run --bin lexgen -- --lexdir $HOME/.ghq/github.com/bluesky-social/atproto/lexicons
+```
+
+Parse AT Protocol schema ([Lexicon](https://atproto.com/guides/lexicon)) and generate Rust code for APIs.
+
+## [atprs-cli](./atprs-cli/README.md)
+
+Command-line client using generated API library.
---
+++ src/bin/lexgen.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+use atprs_codegen::genapi;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    lexdir: PathBuf,
+    #[arg(short, long, default_value = "./atprs-api/src")]
+    outdir: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    for prefix in ["app.bsky", "com.atproto"] {
+        genapi(&args.lexdir, &args.outdir, prefix)?;
+    }
+    Ok(())
+}
