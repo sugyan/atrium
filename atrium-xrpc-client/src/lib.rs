5c9219dcbe45b57bc01e266f8898352a7c1ade67
sugyan <sugi1982@gmail.com>
2024-02-27 00:13:12 +0900

    Fix documentations

--- atrium-xrpc-client/src/lib.rs
+++ atrium-xrpc-client/src/lib.rs
@@ -5,7 +5,7 @@
 #[cfg(feature = "isahc")]
 pub mod isahc;
 #[cfg_attr(docsrs, doc(cfg(feature = "reqwest")))]
-#[cfg(feature = "reqwest")]
+#[cfg(any(feature = "reqwest", target_arch = "wasm32"))]
 pub mod reqwest;
 #[cfg_attr(docsrs, doc(cfg(feature = "surf")))]
 #[cfg(feature = "surf")]
