8fe20a4f47fe5d40f9a3f35142176d870c809302
sugyan <sugi1982@gmail.com>
2024-09-09 22:51:49 +0900

    Add DohDnsTxtResolver as optional

--- atrium-oauth/oauth-client/src/identity/handle.rs
+++ atrium-oauth/oauth-client/src/identity/handle.rs
@@ -1,18 +1,22 @@
 mod appview_resolver;
 mod atproto_resolver;
 mod dns_resolver;
+#[cfg(feature = "doh-handle-resolver")]
+mod doh_dns_txt_resolver;
 mod well_known_resolver;
 
+pub use self::appview_resolver::{AppViewHandleResolver, AppViewHandleResolverConfig};
+pub use self::atproto_resolver::{AtprotoHandleResolver, AtprotoHandleResolverConfig};
+pub use self::dns_resolver::DnsTxtResolver;
+#[cfg(feature = "doh-handle-resolver")]
+pub use self::doh_dns_txt_resolver::{DohDnsTxtResolver, DohDnsTxtResolverConfig};
+pub use self::well_known_resolver::{WellKnownHandleResolver, WellKnownHandleResolverConfig};
 use super::{Error, Resolver, Result};
-pub use appview_resolver::{AppViewHandleResolver, AppViewHandleResolverConfig};
 use async_trait::async_trait;
-pub use atproto_resolver::{AtprotoHandleResolver, AtprotoHandleResolverConfig};
 use atrium_api::types::string::{Did, Handle};
 use atrium_xrpc::HttpClient;
-pub use dns_resolver::DnsTxtResolver;
 use dns_resolver::DynamicDnsTxtResolver;
 use std::sync::Arc;
-pub use well_known_resolver::{WellKnownHandleResolver, WellKnownHandleResolverConfig};
 
 pub trait HandleResolver: Resolver<Input = Handle, Output = Did> {}
 
---
+++ atrium-oauth/oauth-client/src/identity/handle/doh_dns_txt_resolver.rs
@@ -0,0 +1,77 @@
+use super::DnsTxtResolver;
+use async_trait::async_trait;
+use atrium_xrpc::http::{uri::InvalidUri, Uri};
+use atrium_xrpc::HttpClient;
+use hickory_proto::op::{Message, Query};
+use hickory_proto::rr::{RData, RecordType};
+use reqwest::StatusCode;
+use std::sync::Arc;
+use thiserror::Error;
+
+const DOH_MEDIA_TYPE: &str = "application/dns-message";
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("http status: {0:?}")]
+    HttpStatus(StatusCode),
+}
+
+#[derive(Clone, Debug)]
+pub struct DohDnsTxtResolverConfig<T> {
+    pub service_url: String,
+    pub http_client: Arc<T>,
+}
+
+pub struct DohDnsTxtResolver<T> {
+    service_url: Uri,
+    http_client: Arc<T>,
+}
+
+impl<T> DohDnsTxtResolver<T> {
+    #[allow(dead_code)]
+    pub fn new(config: DohDnsTxtResolverConfig<T>) -> core::result::Result<Self, InvalidUri> {
+        Ok(Self {
+            service_url: config.service_url.parse()?,
+            http_client: config.http_client,
+        })
+    }
+}
+
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+impl<T> DnsTxtResolver for DohDnsTxtResolver<T>
+where
+    T: HttpClient + Send + Sync + 'static,
+{
+    async fn resolve(
+        &self,
+        query: &str,
+    ) -> core::result::Result<Vec<String>, Box<dyn std::error::Error + Send + Sync + 'static>> {
+        let mut message = Message::new();
+        message
+            .set_recursion_desired(true)
+            .add_query(Query::query(query.parse()?, RecordType::TXT));
+        let res = self
+            .http_client
+            .send_http(
+                atrium_xrpc::http::Request::builder()
+                    .method(atrium_xrpc::http::Method::POST)
+                    .header(atrium_xrpc::http::header::CONTENT_TYPE, DOH_MEDIA_TYPE)
+                    .uri(&self.service_url)
+                    .body(message.to_vec()?)?,
+            )
+            .await?;
+        if res.status().is_success() {
+            Ok(Message::from_vec(res.body())?
+                .answers()
+                .iter()
+                .filter_map(|answer| match answer.data() {
+                    Some(RData::TXT(txt)) => Some(txt.to_string()),
+                    _ => None,
+                })
+                .collect())
+        } else {
+            Err(Box::new(Error::HttpStatus(res.status())))
+        }
+    }
+}
