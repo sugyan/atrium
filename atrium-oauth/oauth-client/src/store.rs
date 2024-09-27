e173c9760555288afb7e05021cbb22cbe0ae53ed
sugyan <sugi1982@gmail.com>
2024-09-27 22:13:00 +0900

    Fix SimpleStore trait

--- atrium-oauth/oauth-client/src/store.rs
+++ atrium-oauth/oauth-client/src/store.rs
@@ -2,6 +2,7 @@ pub mod memory;
 pub mod state;
 
 use std::error::Error;
+use std::future::Future;
 use std::hash::Hash;
 
 #[cfg_attr(not(target_arch = "wasm32"), trait_variant::make(Send))]
@@ -12,8 +13,8 @@ where
 {
     type Error: Error + Send + Sync + 'static;
 
-    async fn get(&self, key: &K) -> Result<Option<V>, Self::Error>;
-    async fn set(&self, key: K, value: V) -> Result<(), Self::Error>;
-    async fn del(&self, key: &K) -> Result<(), Self::Error>;
-    async fn clear(&self) -> Result<(), Self::Error>;
+    fn get(&self, key: &K) -> impl Future<Output = Result<Option<V>, Self::Error>>;
+    fn set(&self, key: K, value: V) -> impl Future<Output = Result<(), Self::Error>>;
+    fn del(&self, key: &K) -> impl Future<Output = Result<(), Self::Error>>;
+    fn clear(&self) -> impl Future<Output = Result<(), Self::Error>>;
 }
