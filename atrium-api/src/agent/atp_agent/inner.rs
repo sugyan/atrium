0376c1baf0e0f3bbecbc780a0d5753ff107a9b87
sugyan <sugi1982@gmail.com>
2024-11-07 22:33:52 +0900

    Move AtpAgent

--- atrium-api/src/agent/atp_agent/inner.rs
+++ atrium-api/src/agent/atp_agent/inner.rs
@@ -1,4 +1,4 @@
-use super::{Session, SessionStore};
+use super::{AtpSession, AtpSessionStore};
 use crate::did_doc::DidDocument;
 use crate::types::string::Did;
 use crate::types::TryFromUnknown;
@@ -66,7 +66,7 @@ where
 
 impl<S, T> XrpcClient for WrapperClient<S, T>
 where
-    S: SessionStore + Send + Sync,
+    S: AtpSessionStore + Send + Sync,
     T: XrpcClient + Send + Sync,
 {
     fn base_uri(&self) -> String {
@@ -98,7 +98,7 @@ pub struct Client<S, T> {
 
 impl<S, T> Client<S, T>
 where
-    S: SessionStore + Send + Sync,
+    S: AtpSessionStore + Send + Sync,
     T: XrpcClient + Send + Sync,
 {
     pub fn new(store: Arc<Store<S>>, xrpc: T) -> Self {
@@ -213,7 +213,7 @@ where
 
 impl<S, T> Clone for Client<S, T>
 where
-    S: SessionStore + Send + Sync,
+    S: AtpSessionStore + Send + Sync,
     T: XrpcClient + Send + Sync,
 {
     fn clone(&self) -> Self {
@@ -242,7 +242,7 @@ where
 
 impl<S, T> XrpcClient for Client<S, T>
 where
-    S: SessionStore + Send + Sync,
+    S: AtpSessionStore + Send + Sync,
     T: XrpcClient + Send + Sync,
 {
     fn base_uri(&self) -> String {
@@ -288,14 +288,14 @@ impl<S> Store<S> {
     }
 }
 
-impl<S> SessionStore for Store<S>
+impl<S> AtpSessionStore for Store<S>
 where
-    S: SessionStore + Send + Sync,
+    S: AtpSessionStore + Send + Sync,
 {
-    async fn get_session(&self) -> Option<Session> {
+    async fn get_session(&self) -> Option<AtpSession> {
         self.inner.get_session().await
     }
-    async fn set_session(&self, session: Session) {
+    async fn set_session(&self, session: AtpSession) {
         self.inner.set_session(session).await;
     }
     async fn clear_session(&self) {
