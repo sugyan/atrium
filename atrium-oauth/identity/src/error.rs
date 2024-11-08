1b7a4896f90bd1568d7302422b029ba3d43fa02f
avdb13 <avdb@keemail.me>
2024-11-08 22:56:17 +0000

    add error conversions

--- atrium-oauth/identity/src/error.rs
+++ atrium-oauth/identity/src/error.rs
@@ -1,4 +1,5 @@
 use atrium_api::types::string::Did;
+use atrium_common::resolver;
 use atrium_xrpc::http::uri::InvalidUri;
 use atrium_xrpc::http::StatusCode;
 use thiserror::Error;
@@ -33,6 +34,20 @@ pub enum Error {
     SerdeHtmlForm(#[from] serde_html_form::ser::Error),
     #[error(transparent)]
     Uri(#[from] InvalidUri),
+}
+
+impl From<resolver::Error> for Error {
+    fn from(error: resolver::Error) -> Self {
+        match error {
+            resolver::Error::DnsResolver(error) => Error::DnsResolver(error),
+            resolver::Error::Http(error) => Error::Http(error),
+            resolver::Error::HttpClient(error) => Error::HttpClient(error),
+            resolver::Error::HttpStatus(error) => Error::HttpStatus(error),
+            resolver::Error::SerdeJson(error) => Error::SerdeJson(error),
+            resolver::Error::SerdeHtmlForm(error) => Error::SerdeHtmlForm(error),
+            resolver::Error::Uri(error) => Error::Uri(error),
+        }
+    }
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
