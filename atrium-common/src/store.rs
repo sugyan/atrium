b2a6df5d82cef46f8927ef240c18017c94651af6
avdb13 <avdb@keemail.me>
2024-11-09 05:05:20 +0000

    add trait bounds to `Store` error

--- atrium-common/src/store.rs
+++ atrium-common/src/store.rs
@@ -5,12 +5,24 @@ use std::future::Future;
 use std::hash::Hash;
 
 #[cfg_attr(not(target_arch = "wasm32"), trait_variant::make(Send))]
-pub trait Store<K, V>
+pub trait CellStore<V>
+where
+    V: Clone,
+{
+    type Error: Error + Send + Sync + 'static;
+
+    fn get(&self) -> impl Future<Output = Result<Option<V>, Self::Error>>;
+    fn set(&self, value: V) -> impl Future<Output = Result<(), Self::Error>>;
+    fn clear(&self) -> impl Future<Output = Result<(), Self::Error>>;
+}
+
+#[cfg_attr(not(target_arch = "wasm32"), trait_variant::make(Send))]
+pub trait MapStore<K, V>
 where
     K: Eq + Hash,
     V: Clone,
 {
-    type Error: Error;
+    type Error: Error + Send + Sync + 'static;
 
     fn get(&self, key: &K) -> impl Future<Output = Result<Option<V>, Self::Error>>;
     fn set(&self, key: K, value: V) -> impl Future<Output = Result<(), Self::Error>>;
