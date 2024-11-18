f5be54ad549ecd5996d65612776517353bec0535
sugyan <sugi1982@gmail.com>
2024-11-18 12:22:43 +0900

    Update oauth_client::atproto

--- atrium-oauth/oauth-client/src/atproto.rs
+++ atrium-oauth/oauth-client/src/atproto.rs
@@ -1,6 +1,6 @@
 use crate::keyset::Keyset;
 use crate::types::{OAuthClientMetadata, TryIntoOAuthClientMetadata};
-use atrium_xrpc::http::uri::Uri;
+use atrium_xrpc::http::uri::{InvalidUri, Scheme, Uri};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
@@ -20,6 +20,20 @@ pub enum Error {
     AuthSigningAlg,
     #[error(transparent)]
     SerdeHtmlForm(#[from] serde_html_form::ser::Error),
+    #[error(transparent)]
+    LocalhostClient(#[from] LocalhostClientError),
+}
+
+#[derive(Error, Debug)]
+pub enum LocalhostClientError {
+    #[error("invalid redirect_uri: {0}")]
+    Invalid(#[from] InvalidUri),
+    #[error("loopback client_id must use `http:` redirect_uri")]
+    NotHttpScheme,
+    #[error("loopback client_id must not use `localhost` as redirect_uri hostname")]
+    Localhost,
+    #[error("loopback client_id must not use loopback addresses as redirect_uri")]
+    NotLoopbackHost,
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
@@ -107,6 +121,22 @@ impl TryIntoOAuthClientMetadata for AtprotoLocalhostClientMetadata {
     type Error = Error;
 
     fn try_into_client_metadata(self, _: &Option<Keyset>) -> Result<OAuthClientMetadata> {
+        // validate redirect_uris
+        if let Some(redirect_uris) = &self.redirect_uris {
+            for redirect_uri in redirect_uris {
+                let uri = redirect_uri.parse::<Uri>().map_err(LocalhostClientError::Invalid)?;
+                if uri.scheme() != Some(&Scheme::HTTP) {
+                    return Err(Error::LocalhostClient(LocalhostClientError::NotHttpScheme));
+                }
+                if uri.host() == Some("localhost") {
+                    return Err(Error::LocalhostClient(LocalhostClientError::Localhost));
+                }
+                if uri.host().map_or(true, |host| host != "127.0.0.1" && host != "[::1]") {
+                    return Err(Error::LocalhostClient(LocalhostClientError::NotLoopbackHost));
+                }
+            }
+        }
+        // determine client_id
         #[derive(serde::Serialize)]
         struct Parameters {
             #[serde(skip_serializing_if = "Option::is_none")]
@@ -195,6 +225,15 @@ impl TryIntoOAuthClientMetadata for AtprotoClientMetadata {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use elliptic_curve::SecretKey;
+    use jose_jwk::{Jwk, Key, Parameters};
+    use p256::pkcs8::DecodePrivateKey;
+
+    const PRIVATE_KEY: &str = r#"-----BEGIN PRIVATE KEY-----
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgED1AAgC7Fc9kPh5T
+4i4Tn+z+tc47W1zYgzXtyjJtD92hRANCAAT80DqC+Z/JpTO7/pkPBmWqIV1IGh1P
+gbGGr0pN+oSing7cZ0169JaRHTNh+0LNQXrFobInX6cj95FzEdRyT4T3
+-----END PRIVATE KEY-----"#;
 
     #[test]
     fn test_localhost_client_metadata_default() {
@@ -229,12 +268,13 @@ mod tests {
             scopes: Some(vec![
                 Scope::Known(KnownScope::Atproto),
                 Scope::Known(KnownScope::TransitionGeneric),
+                Scope::Unknown(String::from("unknown")),
             ]),
         };
         assert_eq!(
             metadata.try_into_client_metadata(&None).expect("failed to convert metadata"),
             OAuthClientMetadata {
-                client_id: String::from("http://localhost?redirect_uri=http%3A%2F%2F127.0.0.1%2Fcallback&redirect_uri=http%3A%2F%2F%5B%3A%3A1%5D%2Fcallback&scope=atproto+transition%3Ageneric"),
+                client_id: String::from("http://localhost?redirect_uri=http%3A%2F%2F127.0.0.1%2Fcallback&redirect_uri=http%3A%2F%2F%5B%3A%3A1%5D%2Fcallback&scope=atproto+transition%3Ageneric+unknown"),
                 client_uri: None,
                 redirect_uris: vec![
                     String::from("http://127.0.0.1/callback"),
@@ -249,5 +289,108 @@ mod tests {
                 token_endpoint_auth_signing_alg: None,
             }
         );
+    }
+
+    #[test]
+    fn test_localhost_client_metadata_invalid() {
+        {
+            let metadata = AtprotoLocalhostClientMetadata {
+                redirect_uris: Some(vec![String::from("http://")]),
+                ..Default::default()
+            };
+            let err = metadata.try_into_client_metadata(&None).expect_err("expected to fail");
+            assert!(matches!(err, Error::LocalhostClient(LocalhostClientError::Invalid(_))));
+        }
+        {
+            let metadata = AtprotoLocalhostClientMetadata {
+                redirect_uris: Some(vec![String::from("https://127.0.0.1/")]),
+                ..Default::default()
+            };
+            let err = metadata.try_into_client_metadata(&None).expect_err("expected to fail");
+            assert!(matches!(err, Error::LocalhostClient(LocalhostClientError::NotHttpScheme)));
+        }
+        {
+            let metadata = AtprotoLocalhostClientMetadata {
+                redirect_uris: Some(vec![String::from("http://localhost:8000/")]),
+                ..Default::default()
+            };
+            let err = metadata.try_into_client_metadata(&None).expect_err("expected to fail");
+            assert!(matches!(err, Error::LocalhostClient(LocalhostClientError::Localhost)));
+        }
+        {
+            let metadata = AtprotoLocalhostClientMetadata {
+                redirect_uris: Some(vec![String::from("http://192.168.0.0/")]),
+                ..Default::default()
+            };
+            let err = metadata.try_into_client_metadata(&None).expect_err("expected to fail");
+            assert!(matches!(err, Error::LocalhostClient(LocalhostClientError::NotLoopbackHost)));
+        }
+    }
+
+    #[test]
+    fn test_client_metadata() {
+        let metadata = AtprotoClientMetadata {
+            client_id: String::from("https://example.com/client_metadata.json"),
+            client_uri: String::from("https://example.com"),
+            redirect_uris: vec![String::from("https://example.com/callback")],
+            token_endpoint_auth_method: AuthMethod::PrivateKeyJwt,
+            grant_types: vec![GrantType::AuthorizationCode],
+            scopes: vec![Scope::Known(KnownScope::Atproto)],
+            jwks_uri: None,
+            token_endpoint_auth_signing_alg: Some(String::from("ES256")),
+        };
+        {
+            let metadata = metadata.clone();
+            let err = metadata.try_into_client_metadata(&None).expect_err("expected to fail");
+            assert!(matches!(err, Error::EmptyJwks));
+        }
+        {
+            let metadata = metadata.clone();
+            let secret_key = SecretKey::<p256::NistP256>::from_pkcs8_pem(PRIVATE_KEY)
+                .expect("failed to parse private key");
+            let keys = vec![Jwk {
+                key: Key::from(&secret_key.into()),
+                prm: Parameters { kid: Some(String::from("kid00")), ..Default::default() },
+            }];
+            let keyset = Keyset::try_from(keys.clone()).expect("failed to create keyset");
+            assert_eq!(
+                metadata
+                    .try_into_client_metadata(&Some(keyset.clone()))
+                    .expect("failed to convert metadata"),
+                OAuthClientMetadata {
+                    client_id: String::from("https://example.com/client_metadata.json"),
+                    client_uri: Some(String::from("https://example.com")),
+                    redirect_uris: vec![String::from("https://example.com/callback"),],
+                    scope: Some(String::from("atproto")),
+                    grant_types: Some(vec![String::from("authorization_code")]),
+                    token_endpoint_auth_method: Some(AuthMethod::PrivateKeyJwt.into()),
+                    dpop_bound_access_tokens: Some(true),
+                    jwks_uri: None,
+                    jwks: Some(keyset.public_jwks()),
+                    token_endpoint_auth_signing_alg: Some(String::from("ES256")),
+                }
+            );
+        }
+    }
+
+    #[test]
+    fn test_scope_serde() {
+        #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+        struct Scopes {
+            scopes: Vec<Scope>,
+        }
+
+        let scopes = Scopes {
+            scopes: vec![
+                Scope::Known(KnownScope::Atproto),
+                Scope::Known(KnownScope::TransitionGeneric),
+                Scope::Unknown(String::from("unknown")),
+            ],
+        };
+        let json = serde_json::to_string(&scopes).expect("failed to serialize scopes");
+        assert_eq!(json, r#"{"scopes":["atproto","transition:generic","unknown"]}"#);
+        let deserialized =
+            serde_json::from_str::<Scopes>(&json).expect("failed to deserialize scopes");
+        assert_eq!(deserialized, scopes);
     }
 }
