90cd30f71c24eb3855e717ab61d7b9967532ba64
Dr. Chat <arkolbed@gmail.com>
2025-01-28 17:50:01 -0600

    Full CRUD functionality for `Repository`

--- atrium-repo/src/repo.rs
+++ atrium-repo/src/repo.rs
@@ -1,12 +1,15 @@
 use atrium_api::types::{
-    string::{Did, Tid},
-    Collection,
+    string::{Did, RecordKey, Tid},
+    Collection, LimitedU32,
 };
 use ipld_core::{cid::Cid, ipld::Ipld};
 use serde::{Deserialize, Serialize};
 use sha2::Digest;
 
-use crate::{blockstore::AsyncBlockStoreRead, mst};
+use crate::{
+    blockstore::{AsyncBlockStoreRead, AsyncBlockStoreWrite, DAG_CBOR, SHA2_256},
+    mst,
+};
 
 mod schema {
     use super::*;
@@ -64,6 +67,112 @@ async fn read_record<C: Collection>(
     Ok(parsed)
 }
 
+/// A [Commit] builder.
+///
+/// Example usage:
+/// ```ignore
+/// let commit = Commit::new(did, root)
+///     // .rev("1234abcd");
+/// let hash = commit.hash();
+///
+/// // Sign the SHA256 digest however you wish - either using `atrium-crypto` or
+/// // (preferably) using a HSM from a cloud provider.
+/// let signature = todo!();
+///
+/// let commit: Commit = commit.sign(signature);
+/// ```
+pub struct CommitBuilder<'r, S: AsyncBlockStoreWrite> {
+    repo: &'r mut Repository<S>,
+    inner: schema::Commit,
+}
+
+impl<'r, S: AsyncBlockStoreWrite> CommitBuilder<'r, S> {
+    fn new(repo: &'r mut Repository<S>, did: Did, root: Cid) -> Self {
+        CommitBuilder {
+            inner: schema::Commit {
+                did,
+                version: 3,
+                data: root,
+                rev: Tid::now(LimitedU32::MIN),
+                prev: None,
+            },
+            repo,
+        }
+    }
+
+    /// Set the `prev` commit field, which contains a link to the previous commit
+    pub fn prev(&mut self, prev: Cid) -> &mut Self {
+        self.inner.prev = Some(prev);
+        self
+    }
+
+    /// Set the `rev` commit field, which is a monotonically-increasing number that can
+    /// be used to signify the order in which commits are made.
+    pub fn rev(&mut self, time: Tid) -> &mut Self {
+        self.inner.rev = time;
+        self
+    }
+
+    /// Calculate the cryptographic hash of the commit.
+    pub fn hash(&self) -> [u8; 32] {
+        let commit = serde_ipld_dagcbor::to_vec(&self.inner).unwrap(); // This should (hopefully!) never fail
+        sha2::Sha256::digest(commit).into()
+    }
+
+    /// Cryptographically sign the commit, ensuring it can never be mutated again.
+    ///
+    /// We assume that the provided cryptographic hash is valid. If the signature
+    /// is invalid, the commit will be rejected when published to the network!
+    pub async fn sign(self, sig: Vec<u8>) -> Result<Cid, Error> {
+        let s = schema::SignedCommit {
+            did: self.inner.did.clone(),
+            version: self.inner.version,
+            data: self.inner.data,
+            rev: self.inner.rev.clone(),
+            prev: self.inner.prev,
+            sig: Ipld::Bytes(sig),
+        };
+        let b = serde_ipld_dagcbor::to_vec(&s).unwrap();
+        let c = self.repo.db.write_block(DAG_CBOR, SHA2_256, &b).await?;
+
+        self.repo.root = c;
+        self.repo.latest_commit = s.clone();
+        Ok(c)
+    }
+}
+
+/// A [Repository] builder.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RepoBuilder<S: AsyncBlockStoreRead + AsyncBlockStoreWrite> {
+    db: S,
+    commit: schema::Commit,
+}
+
+impl<S: AsyncBlockStoreRead + AsyncBlockStoreWrite> RepoBuilder<S> {
+    /// Get the cryptographic hash of the root commit.
+    pub fn hash(&self) -> [u8; 32] {
+        let commit = serde_ipld_dagcbor::to_vec(&self.commit).unwrap(); // This should (hopefully!) never fail
+        sha2::Sha256::digest(commit).into()
+    }
+
+    /// Cryptographically sign the root commit, finalizing the initial version of this repository.
+    pub async fn sign(mut self, sig: Vec<u8>) -> Result<Repository<S>, Error> {
+        // Write the commit into the database.
+        let s = schema::SignedCommit {
+            did: self.commit.did.clone(),
+            version: self.commit.version,
+            data: self.commit.data,
+            rev: self.commit.rev.clone(),
+            prev: self.commit.prev,
+            sig: Ipld::Bytes(sig),
+        };
+        let b = serde_ipld_dagcbor::to_vec(&s).unwrap();
+        let c = self.db.write_block(DAG_CBOR, SHA2_256, &b).await?;
+
+        Ok(Repository { db: self.db, root: c, latest_commit: s })
+    }
+}
+
 /// A reference to a particular commit to a repository.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Commit {
@@ -103,8 +212,9 @@ impl Commit {
 ///
 /// Reference: https://atproto.com/specs/repository
 #[derive(Debug)]
-pub struct Repository<R: AsyncBlockStoreRead> {
-    db: R,
+pub struct Repository<S> {
+    db: S,
+    root: Cid,
     latest_commit: schema::SignedCommit,
 }
 
@@ -117,17 +227,17 @@ impl<R: AsyncBlockStoreRead> Repository<R> {
         let latest_commit: schema::SignedCommit =
             serde_ipld_dagcbor::from_reader(&commit_block[..])?;
 
-        Ok(Self { db, latest_commit })
+        Ok(Self { db, root, latest_commit })
+    }
+
+    /// Returns the current root cid.
+    pub fn root(&self) -> Cid {
+        self.root
     }
 
     /// Returns the latest commit in the repository.
     pub fn commit(&self) -> Commit {
         Commit { inner: self.latest_commit.clone() }
-    }
-
-    /// Returns the DID for the repository's user.
-    pub fn did(&self) -> &Did {
-        &self.latest_commit.did
     }
 
     /// Returns the specified record from the repository, or `None` if it does not exist.
@@ -152,6 +262,73 @@ impl<R: AsyncBlockStoreRead> Repository<R> {
     }
 }
 
+impl<S: AsyncBlockStoreRead + AsyncBlockStoreWrite> Repository<S> {
+    /// Build a new user repository.
+    pub async fn create(mut db: S, did: Did) -> Result<RepoBuilder<S>, Error> {
+        let tree = mst::Tree::create(&mut db).await?;
+        let root = tree.root();
+
+        Ok(RepoBuilder {
+            db,
+            commit: schema::Commit {
+                did,
+                version: 3,
+                data: root,
+                rev: Tid::now(LimitedU32::MIN),
+                prev: None,
+            },
+        })
+    }
+
+    /// Add a new record to this repository.
+    pub async fn add<'a, C: Collection>(
+        &'a mut self,
+        rkey: RecordKey,
+        record: C::Record,
+    ) -> Result<CommitBuilder<'a, S>, Error> {
+        let path = C::repo_path(&rkey);
+        let data = serde_ipld_dagcbor::to_vec(&record).unwrap();
+        let cid = self.db.write_block(DAG_CBOR, SHA2_256, &data).await?;
+
+        let mut mst = mst::Tree::open(&mut self.db, self.latest_commit.data);
+        mst.add(&path, cid).await?;
+        let root = mst.root();
+
+        Ok(CommitBuilder::new(self, self.latest_commit.did.clone(), root))
+    }
+
+    /// Update an existing record in the repository.
+    pub async fn update<'a, C: Collection>(
+        &'a mut self,
+        rkey: RecordKey,
+        record: C::Record,
+    ) -> Result<CommitBuilder<'a, S>, Error> {
+        let path = C::repo_path(&rkey);
+        let data = serde_ipld_dagcbor::to_vec(&record).unwrap();
+        let cid = self.db.write_block(DAG_CBOR, SHA2_256, &data).await?;
+
+        let mut mst = mst::Tree::open(&mut self.db, self.latest_commit.data);
+        mst.update(&path, cid).await?;
+        let root = mst.root();
+
+        Ok(CommitBuilder::new(self, self.latest_commit.did.clone(), root))
+    }
+
+    /// Delete an existing record in the repository.
+    pub async fn delete<'a, C: Collection>(
+        &'a mut self,
+        rkey: RecordKey,
+    ) -> Result<CommitBuilder<'a, S>, Error> {
+        let path = C::repo_path(&rkey);
+
+        let mut mst = mst::Tree::open(&mut self.db, self.latest_commit.data);
+        mst.delete(&path).await?;
+        let root = mst.root();
+
+        Ok(CommitBuilder::new(self, self.latest_commit.did.clone(), root))
+    }
+}
+
 /// Errors that can occur while interacting with a repository.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -169,8 +346,19 @@ pub enum Error {
 
 #[cfg(test)]
 mod test {
-    use crate::blockstore::CarStore;
-    use atrium_api::types::Object;
+    use std::str::FromStr;
+
+    use crate::blockstore::{CarStore, MemoryBlockStore};
+    use atrium_api::{
+        app::bsky,
+        types::{string::Datetime, Object},
+    };
+    use atrium_crypto::{
+        did::parse_did_key,
+        keypair::{Did as _, P256Keypair},
+        verify::Verifier,
+        Algorithm,
+    };
 
     use super::*;
 
@@ -208,5 +396,69 @@ mod test {
         println!("{:?}", commit.ops);
 
         load(commit.blocks.as_slice()).await.unwrap_err();
+    }
+
+    #[tokio::test]
+    async fn test_create_repo() {
+        let mut bs = MemoryBlockStore::new();
+
+        // Create a signing key pair.
+        let keypair = P256Keypair::create(&mut rand::thread_rng());
+        let dkey = keypair.did();
+
+        // Build a new repository.
+        let builder =
+            Repository::create(&mut bs, Did::new("did:web:pds.abc.com".to_string()).unwrap())
+                .await
+                .unwrap();
+
+        // Sign the root commit.
+        let sig = keypair.sign(&builder.hash()).unwrap();
+
+        // Finalize the root commit and create the repository.
+        let mut repo = builder.sign(sig).await.unwrap();
+        let commit = repo.commit();
+
+        // Ensure that we can verify the root commit.
+        let (_, pub_key) = parse_did_key(&dkey).unwrap();
+        Verifier::default()
+            .verify(Algorithm::P256, &pub_key, &commit.hash(), commit.sig())
+            .unwrap();
+
+        // Ensure the root commit points to the known empty MST CID.
+        assert_eq!(
+            commit.data(),
+            Cid::from_str("bafyreie5737gdxlw5i64vzichcalba3z2v5n6icifvx5xytvske7mr3hpm").unwrap()
+        );
+
+        // Commit a record.
+        let mut cb = repo
+            .add::<bsky::feed::Post>(
+                RecordKey::new(Tid::now(LimitedU32::MIN).to_string()).unwrap(),
+                bsky::feed::post::RecordData {
+                    created_at: Datetime::now(),
+                    embed: None,
+                    entities: None,
+                    facets: None,
+                    labels: None,
+                    langs: None,
+                    reply: None,
+                    tags: None,
+                    text: "Hello world".to_string(),
+                }
+                .into(),
+            )
+            .await
+            .unwrap();
+
+        let sig = keypair.sign(&cb.hash()).unwrap();
+        let cid = cb.sign(sig).await.unwrap();
+
+        // Verify the new commit.
+        let commit = repo.commit();
+
+        Verifier::default()
+            .verify(Algorithm::P256, &pub_key, &commit.hash(), commit.sig())
+            .unwrap();
     }
 }
