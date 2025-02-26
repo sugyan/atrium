0939b0ef52f61196c0f2343c0ee583f114d60900
sugyan <sugi1982@gmail.com>
2025-02-26 23:46:34 +0900

    Remove unused code

--- atrium-oauth/oauth-client/src/oauth_session/store.rs
+++ atrium-oauth/oauth-client/src/oauth_session/store.rs
@@ -2,13 +2,8 @@ use atrium_api::agent::AuthorizationProvider;
 use atrium_common::store::{self, memory::MemoryStore, Store};
 use atrium_xrpc::types::AuthorizationToken;
 
-#[cfg_attr(not(target_arch = "wasm32"), trait_variant::make(Send))]
-pub trait OAuthSessionStore: store::Store<(), String> + AuthorizationProvider {}
-
 #[derive(Default)]
 pub struct MemorySessionStore(MemoryStore<(), String>);
-
-impl OAuthSessionStore for MemorySessionStore {}
 
 impl Store<(), String> for MemorySessionStore {
     type Error = store::memory::Error;
