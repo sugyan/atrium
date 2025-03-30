ef12826ebbc1eb9fa59bfde299809aca4a900218
Dr. Chat <arkolbed@gmail.com>
2025-03-30 12:32:05 -0500

    Remove `paste` since we are no longer using it

--- atrium-api/src/types/integer.rs
+++ atrium-api/src/types/integer.rs
@@ -7,196 +7,194 @@ use serde::{de::Error, Deserialize};
 
 macro_rules! uint {
     ($primitive:ident, $nz:ident, $lim:ident, $lim_nz:ident, $bounded:ident) => {
-        paste::paste! {
-            /// An unsigned integer with a maximum value of `MAX`.
-            #[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, Hash)]
-            #[repr(transparent)]
-            #[serde(transparent)]
-            pub struct $lim<const MAX: $primitive>($primitive);
+        /// An unsigned integer with a maximum value of `MAX`.
+        #[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, Hash)]
+        #[repr(transparent)]
+        #[serde(transparent)]
+        pub struct $lim<const MAX: $primitive>($primitive);
 
-            impl<const MAX: $primitive> $lim<MAX> {
-                /// The smallest value that can be represented by this limited integer type.
-                pub const MIN: Self = Self(<$primitive>::MIN);
+        impl<const MAX: $primitive> $lim<MAX> {
+            /// The smallest value that can be represented by this limited integer type.
+            pub const MIN: Self = Self(<$primitive>::MIN);
 
-                /// The largest value that can be represented by this limited integer type.
-                pub const MAX: Self = Self(MAX);
+            /// The largest value that can be represented by this limited integer type.
+            pub const MAX: Self = Self(MAX);
 
-                fn new(value: $primitive) -> Result<Self, String> {
-                    if value > MAX {
-                        Err(format!("value is greater than {}", MAX))
-                    } else {
-                        Ok(Self(value))
-                    }
+            fn new(value: $primitive) -> Result<Self, String> {
+                if value > MAX {
+                    Err(format!("value is greater than {}", MAX))
+                } else {
+                    Ok(Self(value))
                 }
             }
+        }
 
-            impl<const MAX: $primitive> FromStr for $lim<MAX> {
-                type Err = String;
+        impl<const MAX: $primitive> FromStr for $lim<MAX> {
+            type Err = String;
 
-                fn from_str(src: &str) -> Result<Self, Self::Err> {
-                    Self::new(src.parse::<$primitive>().map_err(|_| format!("value is not an integer"))?)
-                }
+            fn from_str(src: &str) -> Result<Self, Self::Err> {
+                Self::new(src.parse::<$primitive>().map_err(|_| format!("value is not an integer"))?)
             }
+        }
 
-            impl<const MAX: $primitive> TryFrom<$primitive> for $lim<MAX> {
-                type Error = String;
+        impl<const MAX: $primitive> TryFrom<$primitive> for $lim<MAX> {
+            type Error = String;
 
-                fn try_from(value: $primitive) -> Result<Self, Self::Error> {
-                    Self::new(value)
-                }
+            fn try_from(value: $primitive) -> Result<Self, Self::Error> {
+                Self::new(value)
             }
+        }
 
-            impl<'de, const MAX: $primitive> Deserialize<'de> for $lim<MAX> {
-                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-                where
-                    D: serde::Deserializer<'de>,
-                {
-                    Self::new(
-                        Deserialize::deserialize(deserializer)?
-                    ).map_err(D::Error::custom)
-                }
-            }
-
-            impl<const MAX: $primitive> From<$lim<MAX>> for $primitive {
-                fn from(value: $lim<MAX>) -> Self {
-                    value.0
-                }
-            }
-
-            /// An unsigned integer with a minimum value of 1 and a maximum value of `MAX`.
-            #[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, Hash)]
-            #[repr(transparent)]
-            #[serde(transparent)]
-            pub struct $lim_nz<const MAX: $primitive>($nz);
-
-            impl<const MAX: $primitive> $lim_nz<MAX> {
-                /// The smallest value that can be represented by this limited non-zero
-                /// integer type.
-                pub const MIN: Self = Self($nz::MIN);
-
-                /// The largest value that can be represented by this limited non-zero integer
-                /// type.
-                pub const MAX: Self = Self(unsafe { $nz::new_unchecked(MAX) });
-
-                fn new(value: $primitive) -> Result<Self, String> {
-                    if value > MAX {
-                        Err(format!("value is greater than {}", MAX))
-                    } else if let Some(value) = $nz::new(value) {
-                        Ok(Self(value))
-                    } else {
-                        Err("value is zero".into())
-                    }
-                }
-            }
-
-            impl<const MAX: $primitive> FromStr for $lim_nz<MAX> {
-                type Err = String;
-
-                fn from_str(src: &str) -> Result<Self, Self::Err> {
-                    Self::new(src.parse::<$primitive>().map_err(|_| format!("value is not an integer"))?)
-                }
-            }
-
-            impl<const MAX: $primitive> TryFrom<$primitive> for $lim_nz<MAX> {
-                type Error = String;
-
-                fn try_from(value: $primitive) -> Result<Self, Self::Error> {
-                    Self::new(value)
-                }
-            }
-
-            impl<'de, const MAX: $primitive> Deserialize<'de> for $lim_nz<MAX> {
-                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-                where
-                    D: serde::Deserializer<'de>,
-                {
-                    Self::new(
-                        Deserialize::deserialize(deserializer)?
-                    ).map_err(D::Error::custom)
-                }
-            }
-
-            impl<const MAX: $primitive> From<$lim_nz<MAX>> for $nz {
-                fn from(value: $lim_nz<MAX>) -> Self {
-                    value.0
-                }
-            }
-
-            impl<const MAX: $primitive> From<$lim_nz<MAX>> for $primitive {
-                fn from(value: $lim_nz<MAX>) -> Self {
-                    value.0.into()
-                }
-            }
-
-            /// An unsigned integer with a minimum value of `MIN` and a maximum value of `MAX`.
-            ///
-            /// `MIN` must be non-zero.
-            #[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, Hash)]
-            #[repr(transparent)]
-            #[serde(transparent)]
-            pub struct $bounded<const MIN: $primitive, const MAX: $primitive>($nz);
-
-            impl<const MIN: $primitive, const MAX: $primitive> $bounded<MIN, MAX> {
-                /// The smallest value that can be represented by this bounded integer type.
-                pub const MIN: Self = Self(unsafe { $nz::new_unchecked(MIN) });
-
-                /// The largest value that can be represented by this bounded integer type.
-                pub const MAX: Self = Self(unsafe { $nz::new_unchecked(MAX) });
-
-                fn new(value: $primitive) -> Result<Self, String> {
-                    if value < MIN {
-                        Err(format!("value is less than {}", MIN))
-                    } else if value > MAX {
-                        Err(format!("value is greater than {}", MAX))
-                    } else if let Some(value) = $nz::new(value) {
-                        Ok(Self(value))
-                    } else {
-                        Err("value is zero".into())
-                    }
-                }
-            }
-
-            impl<const MIN: $primitive, const MAX: $primitive> TryFrom<$primitive>
-                for $bounded<MIN, MAX>
+        impl<'de, const MAX: $primitive> Deserialize<'de> for $lim<MAX> {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
             {
-                type Error = String;
+                Self::new(
+                    Deserialize::deserialize(deserializer)?
+                ).map_err(D::Error::custom)
+            }
+        }
 
-                fn try_from(value: $primitive) -> Result<Self, Self::Error> {
-                    Self::new(value)
+        impl<const MAX: $primitive> From<$lim<MAX>> for $primitive {
+            fn from(value: $lim<MAX>) -> Self {
+                value.0
+            }
+        }
+
+        /// An unsigned integer with a minimum value of 1 and a maximum value of `MAX`.
+        #[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, Hash)]
+        #[repr(transparent)]
+        #[serde(transparent)]
+        pub struct $lim_nz<const MAX: $primitive>($nz);
+
+        impl<const MAX: $primitive> $lim_nz<MAX> {
+            /// The smallest value that can be represented by this limited non-zero
+            /// integer type.
+            pub const MIN: Self = Self($nz::MIN);
+
+            /// The largest value that can be represented by this limited non-zero integer
+            /// type.
+            pub const MAX: Self = Self(unsafe { $nz::new_unchecked(MAX) });
+
+            fn new(value: $primitive) -> Result<Self, String> {
+                if value > MAX {
+                    Err(format!("value is greater than {}", MAX))
+                } else if let Some(value) = $nz::new(value) {
+                    Ok(Self(value))
+                } else {
+                    Err("value is zero".into())
                 }
             }
+        }
 
-            impl<const MIN: $primitive, const MAX: $primitive> FromStr for $bounded<MIN, MAX> {
-                type Err = String;
+        impl<const MAX: $primitive> FromStr for $lim_nz<MAX> {
+            type Err = String;
 
-                fn from_str(src: &str) -> Result<Self, Self::Err> {
-                    Self::new(src.parse::<$primitive>().map_err(|_| format!("value is not an integer"))?)
-                }
+            fn from_str(src: &str) -> Result<Self, Self::Err> {
+                Self::new(src.parse::<$primitive>().map_err(|_| format!("value is not an integer"))?)
             }
+        }
 
-            impl<'de, const MIN: $primitive, const MAX: $primitive> Deserialize<'de>
-                for $bounded<MIN, MAX>
+        impl<const MAX: $primitive> TryFrom<$primitive> for $lim_nz<MAX> {
+            type Error = String;
+
+            fn try_from(value: $primitive) -> Result<Self, Self::Error> {
+                Self::new(value)
+            }
+        }
+
+        impl<'de, const MAX: $primitive> Deserialize<'de> for $lim_nz<MAX> {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
             {
-                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-                where
-                    D: serde::Deserializer<'de>,
-                {
-                    Self::new(
-                        Deserialize::deserialize(deserializer)?
-                    ).map_err(D::Error::custom)
+                Self::new(
+                    Deserialize::deserialize(deserializer)?
+                ).map_err(D::Error::custom)
+            }
+        }
+
+        impl<const MAX: $primitive> From<$lim_nz<MAX>> for $nz {
+            fn from(value: $lim_nz<MAX>) -> Self {
+                value.0
+            }
+        }
+
+        impl<const MAX: $primitive> From<$lim_nz<MAX>> for $primitive {
+            fn from(value: $lim_nz<MAX>) -> Self {
+                value.0.into()
+            }
+        }
+
+        /// An unsigned integer with a minimum value of `MIN` and a maximum value of `MAX`.
+        ///
+        /// `MIN` must be non-zero.
+        #[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, Hash)]
+        #[repr(transparent)]
+        #[serde(transparent)]
+        pub struct $bounded<const MIN: $primitive, const MAX: $primitive>($nz);
+
+        impl<const MIN: $primitive, const MAX: $primitive> $bounded<MIN, MAX> {
+            /// The smallest value that can be represented by this bounded integer type.
+            pub const MIN: Self = Self(unsafe { $nz::new_unchecked(MIN) });
+
+            /// The largest value that can be represented by this bounded integer type.
+            pub const MAX: Self = Self(unsafe { $nz::new_unchecked(MAX) });
+
+            fn new(value: $primitive) -> Result<Self, String> {
+                if value < MIN {
+                    Err(format!("value is less than {}", MIN))
+                } else if value > MAX {
+                    Err(format!("value is greater than {}", MAX))
+                } else if let Some(value) = $nz::new(value) {
+                    Ok(Self(value))
+                } else {
+                    Err("value is zero".into())
                 }
             }
+        }
 
-            impl<const MIN: $primitive, const MAX: $primitive> From<$bounded<MIN, MAX>> for $nz {
-                fn from(value: $bounded<MIN, MAX>) -> Self {
-                    value.0
-                }
+        impl<const MIN: $primitive, const MAX: $primitive> TryFrom<$primitive>
+            for $bounded<MIN, MAX>
+        {
+            type Error = String;
+
+            fn try_from(value: $primitive) -> Result<Self, Self::Error> {
+                Self::new(value)
             }
+        }
 
-            impl<const MIN: $primitive, const MAX: $primitive> From<$bounded<MIN, MAX>> for $primitive {
-                fn from(value: $bounded<MIN, MAX>) -> Self {
-                    value.0.into()
-                }
+        impl<const MIN: $primitive, const MAX: $primitive> FromStr for $bounded<MIN, MAX> {
+            type Err = String;
+
+            fn from_str(src: &str) -> Result<Self, Self::Err> {
+                Self::new(src.parse::<$primitive>().map_err(|_| format!("value is not an integer"))?)
+            }
+        }
+
+        impl<'de, const MIN: $primitive, const MAX: $primitive> Deserialize<'de>
+            for $bounded<MIN, MAX>
+        {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                Self::new(
+                    Deserialize::deserialize(deserializer)?
+                ).map_err(D::Error::custom)
+            }
+        }
+
+        impl<const MIN: $primitive, const MAX: $primitive> From<$bounded<MIN, MAX>> for $nz {
+            fn from(value: $bounded<MIN, MAX>) -> Self {
+                value.0
+            }
+        }
+
+        impl<const MIN: $primitive, const MAX: $primitive> From<$bounded<MIN, MAX>> for $primitive {
+            fn from(value: $bounded<MIN, MAX>) -> Self {
+                value.0.into()
             }
         }
     };
